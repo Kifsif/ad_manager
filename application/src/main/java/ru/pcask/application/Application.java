8f36fb14129ea1687a833edb54534cb8253abf25
Your Name <you@example.com>
2020-10-06 21:51:47 +0300

    tmp

--- application/src/main/java/ru/pcask/application/Application.java
+++ application/src/main/java/ru/pcask/application/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = {//"ru.pcask.client",
+@SpringBootApplication(scanBasePackages = {"ru.pcask.client",
                                             "ru.pcask.activities_type"
                                            })
 public class Application {
