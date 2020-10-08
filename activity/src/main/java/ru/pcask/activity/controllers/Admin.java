f1d1bd03a6ee399f984d895c2b2e7ecbf2bcdd68
Your Name <you@example.com>
2020-10-08 13:04:28 +0300

    tmp

--- activity/src/main/java/ru/pcask/activity/controllers/Admin.java
+++ activity/src/main/java/ru/pcask/activity/controllers/Admin.java
@@ -1,4 +1,4 @@
-package ru.pcask.activities_type.controllers;
+package ru.pcask.activity.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- client/src/main/java/ru/pcask/client/entities/Client.java
+++ client/src/main/java/ru/pcask/client/entities/Client.java
@@ -1,16 +1,17 @@
 package ru.pcask.client.entities;
+import ru.pcask.activity.entities.Activity;
 import javax.persistence.*;
 
 
+
 @Entity
-public class AdminEntity {
+public class Client {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String companyName; // Cокращенное фирменное наименование. Например, ООО "Ромашка".
     private String comment;
 
-    @OneToMany(mappedBy = "activity", fetch = FetchType.LAZY)
-    private Set<Book> bookList;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Activity activity;
 }
