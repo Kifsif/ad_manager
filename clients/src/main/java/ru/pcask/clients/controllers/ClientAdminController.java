07238fe5cf0a66a500fb08ba0cf4a16fb85ac9fb
Your Name <you@example.com>
2020-10-15 13:53:46 +0300

    tmp

--- clients/src/main/java/ru/pcask/clients/controllers/ClientAdminController.java
+++ clients/src/main/java/ru/pcask/clients/controllers/ClientAdminController.java
@@ -1,17 +1,31 @@
 package ru.pcask.clients.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import ru.pcask.clients.entities.Client;
+import ru.pcask.clients.repositories.ClientRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Controller
 @RequestMapping("/clients")
 @ResponseBody
 public class ClientAdminController {
+    @Autowired
+    private ClientRepository clientRepository;
+
     @GetMapping("/admin/")
     public String admin(){
-        return "clients";
+        Iterable<Client> clients = clientRepository.findAll();
+        String tmp = "clients";
+        if (clients != null) {
+            tmp = clients.toString();
+        }
+        return tmp;
     }
 }
--- clients/src/main/java/ru/pcask/clients/repositories/ClientRepository.java
+++ clients/src/main/java/ru/pcask/clients/repositories/ClientRepository.java
@@ -3,8 +3,5 @@ import ru.pcask.clients.entities.Client;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ClientRepository extends CrudRepository<Client, Long> {
-    Client findById(Long id);
-//    Client findByName(String companyName);
-//    Client saveClient(Client a_client);
-//    void deleteClient(Client a_client);
+    Client findByCompanyName(String companyName);
 }
